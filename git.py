a0ababa9890665d2738188056cf5ac1adb642053
blacwolf4 <blacwolf4@gmail.com>
2023-10-02 18:54:17 +0000

    mixed lists

---
+++ git.py
@@ -0,0 +1,15 @@
+#!/usr/env/python3
+
+#This script will sync files to github with user input for project name
+
+
+
+
+name=input("What is the name of this project?\n>")
+
+# This is what you should see when print runs-
+# Hi <name>! Welcome to Day 2 of Python Training!
+
+print("Hi, ", name.capitalize() , "! Welcome to Day ", mylist[0] , " of " , mylist[5] ," Training!")
+
+#print("Hi " + name.capitalize + "! Welcome to Day " + mylist[1] + " of " + mylist[6] " Training!")
