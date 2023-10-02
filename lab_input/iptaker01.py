58b53d7f987c43e98e3faf8459e7028319a597b6
blacwolf4 <blacwolf4@gmail.com>
2023-10-02 16:44:31 +0000

    join and split

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+print() - display data to std out"""
+
+#below is a function containing the code
+
+def main():
+    #pause the program and wait for useer input
+    user_input = input("Please enter an IPv4 address")
+
+    #display the input back to the user
+    print("You told me the IPv4 address is: " + user_input)
+
+
+#this calls main
+main()
