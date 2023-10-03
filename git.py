e4611c773f755c26c2b25268d666dd766aa1132a
blacwolf4 <blacwolf4@gmail.com>
2023-10-03 14:41:42 +0000

    learning dictionaries

--- git.py
+++ git.py
@@ -10,6 +10,16 @@ name=input("What is the name of this project?\n>")
 # This is what you should see when print runs-
 # Hi <name>! Welcome to Day 2 of Python Training!
 
-print("Hi, ", name.capitalize() , "! Welcome to Day ", mylist[0] , " of " , mylist[5] ," Training!")
+git add *
+git commit -m "(name)"
+sleep(3)
+git push origin HEAD
+
+
+#print("Hi, ", name.capitalize() , "! Welcome to Day ", mylist[0] , " of " , mylist[5] ," Training!")
 
 #print("Hi " + name.capitalize + "! Welcome to Day " + mylist[1] + " of " + mylist[6] " Training!")
+
+
+
+
---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+input(f'What is your {name}?')
+
+wordtype=["noun","adjective","verb","another noun"]
+
+for word in wordtype:
+    input_word=input(f"Provide me with a {word}.")
+    print(input_word)
+
+
+
