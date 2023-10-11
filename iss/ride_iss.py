5d114e75616599196ce4284d40ec94636773e618
blacwolf4 <blacwolf4@gmail.com>
2023-10-11 18:55:44 +0000

    ansible api into

--- iss/ride_iss.py
+++ iss/ride_iss.py
@@ -1,40 +1,40 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Alta3 Research | RZFeeser
+   Accessing Open APIs with Python"""
 
-import urllib.request
+# standard library
 import json
 
-MAJORTOM = "http://api.open-notify.org/astros.json"
+# 3rd party library
+import requests
+
+# Define URL as a global constant (this will not change)
+MAJORTOM = 'http://api.open-notify.org/astros.json'
 
 def main():
-    groundctrl = urllib.request.urlopen(MAJORTOM)
+    """making API requests"""
+    
+    # Call the web service
+    resp = requests.get(MAJORTOM)  # sends an HTTP GET
+    
+    # strip JSON data off response and convert
+    # to python data types
+    data = resp.json()
+            
+    # display our Pythonic data
+    print("\n\nConverted Python data")
+    print(data)
+    
+    print('\n\nPeople in Space: ', data.get('number'))
+    
+    astros = data.get('people') # people is a list of dict
+    print(astros) # this is the list of dict
 
-    helmet = groundctrl.read()
+    # for-loop across astros
+    # display names of those in space
+    for astro in astros:
+        print(f"Riding on the {astro['craft']} is:", astro['name'])
 
-    print(helmet)
-
-    helmetson = json.loads(helmet.decode("utf-8"))
-
-    print(type(helmet))
-
-    print(type(helmetson))
-
-    print(helmetson["number"])
-
-    print(helmetson["people"])
-
-    print(helmetson["people"][0])
-
-    print(helmetson["people"][1])
-
-    print(helmetson["people"][-1])
-
-    for astro in helmetson["people"]:
-        print(astro)
-
-    for astro in helmetson["people"]:
-        print(astro["name"])
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
 
